caecc221ec0ef099b19e29aea76cd270151d9083
Tsivilyova <tsivilyova@magora-systems.com>
2020-02-28 12:22:01 +0700

    KNGA-54 исправлено кэширование, отображение фрагмента в центре экрана

--- app/src/main/java/com/hfad/news/tsivileva/newschannel/view/fragments/FragmentFeedContent.kt
+++ app/src/main/java/com/hfad/news/tsivileva/newschannel/view/fragments/FragmentFeedContent.kt
@@ -7,7 +7,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -37,7 +36,6 @@ class FragmentFeedContent :
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         viewModel = ViewModelProviders.of(activity!!).get(FeedDetailsViewModel::class.java)
-
         viewModel.newsItem.observe(viewLifecycleOwner, Observer {
             logIt("FragmentFeedContent", " viewModel.newsStore.observe", "был загружен элемент ${it.id}")
             if (!news.isEmpty())
@@ -54,11 +52,20 @@ class FragmentFeedContent :
                 }
             } else {
                 view?.news_content_progress_bar?.visibility = View.VISIBLE
+                showErrorFragment(
+                        fragmentManager = childFragmentManager,
+                        containerEmptyCache =R.id.news_details_empty_cache_error_container,
+                        containerFullCache =  R.id.news_details_full_cache_error_container,
+                        cachedNews = news ,
+                        tag = FEED_CONTENT_ERROR_DOWNLOADING
+                )
                 DialogNetworkError().show(childFragmentManager, DIALOG_WITH_ERROR)
             }
         })
         return inflater.inflate(R.layout.fragment_feed_details, container, false)
     }
+
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -75,28 +82,31 @@ class FragmentFeedContent :
 
     override fun onDestroyView() {
         super.onDestroyView()
-        viewModel.refreshData()
+         viewModel.refreshData()
     }
 
     private fun showNews(newsItem: NewsItem?) {
-        logIt("FragmentFeedContent", " showNews", "показать элемент : $newsItem")
-        view?.news_content_container?.visibility = View.VISIBLE
-        view?.news_content_progress_bar?.visibility = View.GONE
-        view?.new_details_car_view?.visibility = View.VISIBLE
-        view?.news_details_text_view?.text = newsItem?.content
-        view?.news_details_date_text_view?.text = newsItem?.getStringDate()
-        view?.news_details_title_text_view?.text = newsItem?.title
-        view?.news_details_link_text_view?.text = newsItem?.link
-
-        val path = newsItem?.picture
-        if (path != null && !path.isEmpty()) {
+        if(newsItem?.id!=null){
+            logIt("FragmentFeedContent", " showNews", "показать элемент : $newsItem")
+            view?.news_content_container?.visibility = View.VISIBLE
+            view?.news_content_progress_bar?.visibility = View.GONE
             view?.new_details_car_view?.visibility = View.VISIBLE
-            Picasso.get().load(path).placeholder(R.drawable.no_photo)
-                    .error(R.drawable.no_photo)
-                    .into(view?.news_details_image_view);
-        } else {
-            view?.new_details_car_view?.visibility = View.GONE
+            view?.news_details_text_view?.text = newsItem?.content
+            view?.news_details_date_text_view?.text = newsItem?.getStringDate()
+            view?.news_details_title_text_view?.text = newsItem?.title
+            view?.news_details_link_text_view?.text = newsItem?.link
+
+            val path = newsItem?.picture
+            if (path != null && !path.isEmpty()) {
+                view?.new_details_car_view?.visibility = View.VISIBLE
+                Picasso.get().load(path).placeholder(R.drawable.no_photo)
+                        .error(R.drawable.no_photo)
+                        .into(view?.news_details_image_view);
+            } else {
+                view?.new_details_car_view?.visibility = View.GONE
+            }
         }
+
     }
 
     override fun onDialogReloadClick(dialogNetwork: DialogNetworkError) {
@@ -110,7 +120,9 @@ class FragmentFeedContent :
         view?.news_content_progress_bar?.visibility = View.GONE
         showErrorFragment(
                 fragmentManager = childFragmentManager,
-                containerId = R.id.news_details_error_container,
+                containerEmptyCache =R.id.news_details_empty_cache_error_container,
+                containerFullCache =  R.id.news_details_full_cache_error_container,
+                cachedNews = news ,
                 tag = FEED_CONTENT_ERROR_DOWNLOADING
         )
     }
