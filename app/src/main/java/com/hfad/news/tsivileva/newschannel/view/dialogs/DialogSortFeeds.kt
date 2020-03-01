ac8efd98bb3864333a8800df529203df91b6ad48
LOLyta1 <nata_tsivileva@mail.ru>
2020-03-02 00:27:31 +0600

    KNGA-61 #comment

--- app/src/main/java/com/hfad/news/tsivileva/newschannel/view/dialogs/DialogSortFeeds.kt
+++ app/src/main/java/com/hfad/news/tsivileva/newschannel/view/dialogs/DialogSortFeeds.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.FragmentManager
+import com.hfad.news.tsivileva.newschannel.FeedsSource
 import com.hfad.news.tsivileva.newschannel.R
 import com.hfad.news.tsivileva.newschannel.view_model.Sort
 import kotlinx.android.synthetic.main.dialog_sort_feeds.view.*
@@ -13,7 +14,7 @@ import kotlinx.android.synthetic.main.dialog_sort_feeds.view.*
 class DialogSortFeeds : DialogFragment() {
 
     interface IDialogSortFeedsClickListener {
-        fun onDialogSortClick(sortKind: Sort)
+        fun onDialogSortClick(filter: FeedsSource, sortKind: Sort)
     }
 
     private var listener: IDialogSortFeedsClickListener? = null
@@ -25,15 +26,33 @@ class DialogSortFeeds : DialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         listener = parentFragment as IDialogSortFeedsClickListener
+
+
+
         view.dialog_sort_feed_button.setOnClickListener {
+            var filter: FeedsSource = FeedsSource.BOTH
+            var sort: Sort = Sort.BY_ABC_ASC
+
+            if (view.dialog_proger_check_box.isChecked && view.dialog_habr_check_box.isChecked) {
+                filter = FeedsSource.BOTH
+            } else
+                if (view.dialog_habr_check_box.isChecked) {
+                    filter = FeedsSource.HABR
+                } else
+                    if (view.dialog_proger_check_box.isChecked) {
+                        filter = FeedsSource.PROGER
+                    }
+
             when (view.sort_feeds_radio_group.checkedRadioButtonId) {
-                R.id.sort_feeds_by_abc_asc_radio_button -> listener?.onDialogSortClick(Sort.BY_ABC_ASC)
-                R.id.sort_feeds_by_abs_desc_radio_button -> listener?.onDialogSortClick(Sort.BY_ABC_DESC)
-                R.id.sort_feeds_by_date_asc_radio_button -> listener?.onDialogSortClick(Sort.BY_DATE_ASC)
-                R.id.sort_feeds_by_date_desc_radio_button -> listener?.onDialogSortClick(Sort.BY_DATE_DESC)
+                R.id.sort_feeds_by_abc_asc_radio_button -> sort = Sort.BY_ABC_ASC
+                R.id.sort_feeds_by_abs_desc_radio_button -> sort = Sort.BY_ABC_DESC
+                R.id.sort_feeds_by_date_asc_radio_button -> sort = Sort.BY_DATE_ASC
+                R.id.sort_feeds_by_date_desc_radio_button -> sort = Sort.BY_DATE_DESC
             }
             dismiss()
+            listener?.onDialogSortClick(filter, sort)
         }
+
 
     }
 
--- app/src/main/java/com/hfad/news/tsivileva/newschannel/view/fragments/FragmentFeeds.kt
+++ app/src/main/java/com/hfad/news/tsivileva/newschannel/view/fragments/FragmentFeeds.kt
@@ -5,7 +5,6 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.view.*
 import android.widget.EditText
-import android.widget.SearchView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -18,8 +17,6 @@ import com.hfad.news.tsivileva.newschannel.adapter.NewsListDecorator
 import com.hfad.news.tsivileva.newschannel.repository.DownloadedFeeds
 import com.hfad.news.tsivileva.newschannel.repository.DownloadingError
 import com.hfad.news.tsivileva.newschannel.repository.DownloadingProgress
-import com.hfad.news.tsivileva.newschannel.repository.DownloadingSuccessful
-import com.hfad.news.tsivileva.newschannel.view.dialogs.DialogFilterFeeds
 import com.hfad.news.tsivileva.newschannel.view.dialogs.DialogNetworkError
 import com.hfad.news.tsivileva.newschannel.view.dialogs.DialogSortFeeds
 import com.hfad.news.tsivileva.newschannel.view_model.FeedViewModel
@@ -31,8 +28,7 @@ class FragmentFeeds() :
         NewsListAdapter.INewsItemClickListener,
         DialogNetworkError.IDialogListener,
         FragmentNetworkError.IErrorFragmentListener,
-        DialogSortFeeds.IDialogSortFeedsClickListener,
-        DialogFilterFeeds.IDialogFilterFeedsListener {
+        DialogSortFeeds.IDialogSortFeedsClickListener {
 
     private lateinit var viewModel: FeedViewModel
     private var feeds = listOf<NewsItem>()
@@ -58,19 +54,19 @@ class FragmentFeeds() :
         viewModel.downloadFeeds()
 
         viewModel.downloading.observe(viewLifecycleOwner, Observer {
-            when(it){
-                is DownloadedFeeds->{
+            when (it) {
+                is DownloadedFeeds -> {
                     if (it.feedList.isNotEmpty()) {
                         feeds = it.feedList
                         recyclerAdapter.list = feeds
-                        view.feeds_error_container.visibility=View.GONE
+                        view.feeds_error_container.visibility = View.GONE
                         view.swipe_container?.isRefreshing = false
                     }
                 }
                 is DownloadingError -> {
-                        view.feeds_error_container.visibility=View.VISIBLE
-                        DialogNetworkError().show(childFragmentManager, DIALOG_WITH_ERROR)
-                        view.swipe_container?.isRefreshing = true
+                    view.feeds_error_container.visibility = View.VISIBLE
+                    DialogNetworkError().show(childFragmentManager, DIALOG_WITH_ERROR)
+                    view.swipe_container?.isRefreshing = true
                 }
                 is DownloadingProgress -> {//it.message
                 }
@@ -94,7 +90,6 @@ class FragmentFeeds() :
     }
 
 
-
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.menu_feed_fragment, menu)
         super.onCreateOptionsMenu(menu, inflater)
@@ -107,20 +102,18 @@ class FragmentFeeds() :
                 viewModel.cleareCache()
                 viewModel.downloadFeeds()
             }
-            R.id.filter_feeds_item_menu -> {
-                DialogFilterFeeds().show(childFragmentManager, DIALOG_WITH_FILTER)
-            }
             R.id.sort_feeds_item_menu -> {
                 DialogSortFeeds().show(childFragmentManager, DIALOG_WITH_SORT)
             }
             R.id.app_bar_search -> {
-                val searchField = (item.actionView as EditText).apply{setHint(R.string.search_text)
+                val searchField = (item.actionView as EditText).apply {
+                    setHint(R.string.search_text)
                 }
                 searchField.addTextChangedListener(object : TextWatcher {
                     override fun afterTextChanged(s: Editable?) {}
                     override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
                     override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                            recyclerAdapter.list = viewModel.searchByTitle(s.toString())
+                        recyclerAdapter.list = viewModel.searchByTitle(s.toString())
                     }
                 })
             }
@@ -154,7 +147,7 @@ class FragmentFeeds() :
 
     override fun onDialogErrorCancelClick(dialogNetwork: DialogNetworkError) {
         dialogNetwork.dismiss()
-        view?.feeds_error_container?.visibility=View.VISIBLE
+        view?.feeds_error_container?.visibility = View.VISIBLE
         view?.swipe_container?.isRefreshing = false
     }
 
@@ -163,17 +156,15 @@ class FragmentFeeds() :
         viewModel.cleareCache()
         viewModel.downloadFeeds()
         view?.swipe_container?.isRefreshing = true
-        view?.feeds_error_container?.visibility=View.GONE
+        view?.feeds_error_container?.visibility = View.GONE
     }
 
-    override fun onDialogSortClick(sortKind: Sort) {
-        val _list = mutableListOf<NewsItem>().apply { addAll(recyclerAdapter.list) }
-        viewModel.sortNews(sortKind)
+
+    override fun onDialogSortClick(filter: FeedsSource, sortKind: Sort) {
+        var _list = viewModel.filterNews(filter)
+        _list = viewModel.sortNews(sortKind, _list.toMutableList())
         recyclerAdapter.list = _list
-    }
 
-    override fun onDialogFilterButtonClick(sourceKind: FeedsSource) {
-        recyclerAdapter.list = viewModel.filterNews(sourceKind)
     }
 }
 
